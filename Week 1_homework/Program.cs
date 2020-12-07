581736d873c4e7a58248bbb4df99b4ad45e62fa3
KatyaDremliuh <nerona.91@gmail.com>
2020-12-07 14:07:57 +0300

    Initial commit

---
+++ Week 1_homework/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Week_1_homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            decimal TheFirstAddVariable = 6M; // 6
+            decimal TheSecondAddVariable = TheFirstAddVariable * TheFirstAddVariable; // квадрат 6-ти
+            decimal TheThirdAddVariable = TheSecondAddVariable - 1; // квадрат 6-ти - 1
+            decimal x = TheThirdAddVariable / 2; // искомое значение (квадрат 6-ти - 1)/2
+            
+
+            Console.WriteLine($"Результат вычисления : {x}");
+        }
+    }
+}
